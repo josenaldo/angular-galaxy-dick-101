118c3763117b068b9bbf572b7047657b418c766d
josenaldo <josenaldo@gmail.com>
2015-11-28 14:35:39 -0200

    Aprenda aqui, Sr. Curioso, como chamar várias funções pela diretiva ng-click.
    ps: Porque diabo desse computador nâo realiza com commit com o atalho Shift + enter???

--- hello-form/form-controller.js
+++ hello-form/form-controller.js
@@ -9,11 +9,20 @@
 
     function FormController($scope) {
         var vm = this;
+        vm.mudarNome= mudarNome;
 
         activate();
 
         function activate() {
+            vm.name="Dadinho";
+        }
 
+        function mudarNome(novoNome) {
+            if (novoNome) {
+                vm.name=novoNome;
+            } else {
+                vm.name="Dadinho é o caralho, meu nome é Zé Pequeno, porra.";
+            }
         }
     }
 })();
